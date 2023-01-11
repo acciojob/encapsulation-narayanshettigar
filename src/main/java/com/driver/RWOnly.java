927e27177106ba22ebf6469d34cc50942d96c353
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-01-12 00:12:29 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+
+// RWOnly.java
+
 public class RWOnly {
+    private String name;
     
+    // Task 5
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
