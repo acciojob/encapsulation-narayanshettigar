af6ad0c60cd4efb36e639c54f43c8e522396b26f
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-01-12 00:11:53 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+
+// Main.java
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3
+        RWOnly obj = new RWOnly();
+        // Task 4
+        // this will give error, because the variable is private
+        // obj.name = "John Doe"; 
+        // System.out.println(obj.name);
+
+        // Task 5
+        obj.setName("John Doe");
+        // Task 6
+        System.out.println(obj.getName()); // Output: "John Doe"
+    }
 }
